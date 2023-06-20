9132e077b03d16033e78f2874873a79e55e08fdb
Vinit Gore <vinitgore1234@gmail.com>
2023-06-20 14:06:47 +0530

    Added validations for tasks

---
+++ taskapp/src/main/java/com/fssa/learnJava/project/taskapp/validation/InvalidTaskException.java
@@ -0,0 +1,20 @@
+package com.fssa.learnJava.project.taskapp.validation;
+
+public class InvalidTaskException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public InvalidTaskException(String msg) {
+		super(msg);
+	}
+
+	public InvalidTaskException(Throwable ex) {
+		super(ex);
+	}
+
+	public InvalidTaskException(String msg, Throwable ex) {
+		super(msg, ex);
+	}
+}
---
+++ taskapp/src/main/java/com/fssa/learnJava/project/taskapp/validation/TaskValidator.java
@@ -0,0 +1,19 @@
+package com.fssa.learnJava.project.taskapp.validation;
+
+import com.fssa.learnJava.project.taskapp.model.Task;
+
+public class TaskValidator {
+
+	public boolean validateNewTask(Task task) throws InvalidTaskException {
+		if (task == null) {
+			throw new InvalidTaskException("Problem in creating new task");
+		} else if (task.getTask().isEmpty()) {
+			throw new InvalidTaskException("Task is empty");
+		} else if (task.getTaskStatus() != "PENDING" || task.getTaskStatus() != "COMPLETED") {
+			throw new InvalidTaskException("TaskStatus should be either 'PENDING' or 'COMPLETED'");
+		} else {
+			return true;
+		}
+
+	}
+}
